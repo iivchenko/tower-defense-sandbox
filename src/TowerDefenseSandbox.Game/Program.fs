8ff74b6842c14b10a833a1aff79c6f972e06a8cc
iivchenko <iivchenko@live.com>
2019-12-02 20:45:44 +0100

    #1 Create basic monogame+fsharp project

---
+++ src/TowerDefenseSandbox.Game/Program.fs
@@ -0,0 +1,15 @@
+﻿open Microsoft.Xna.Framework
+open System
+
+type TheGame () as this =
+    inherit Microsoft.Xna.Framework.Game()
+
+    let graphics = new GraphicsDeviceManager(this)
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv =
+    let game = new TheGame()
+    game.Run()
+
+    0 // return an integer exit code
