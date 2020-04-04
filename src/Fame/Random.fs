ddb4938e4c458ce91636c1984cab12b2e41e4bcb
iivchenko <iivchenko@live.com>
2020-04-04 22:10:53 +0200

    #57 Implement Random module

---
+++ src/Fame/Random.fs
@@ -0,0 +1,11 @@
+﻿module Fame.Random
+
+let mutable private r = System.Random()
+
+let init seed =
+    match seed with  
+    | None -> r <- System.Random()
+    | Some value -> r <- System.Random (value)
+
+let random min max = r.Next(min, max)
+
